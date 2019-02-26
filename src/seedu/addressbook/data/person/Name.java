7952fb1dc37abe51600739017cbd00fec96f1ffb
Yeo Cheng Hong <yeo.ch@u.nus.edu>
2019-02-26 13:45:36 +0800

    Name: add compareTo method

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Represents a Person's name in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
-public class Name {
+public class Name implements Comparable<Name>{
 
     public static final String EXAMPLE = "John Doe";
     public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphanumeric characters";
@@ -47,6 +47,14 @@ public class Name {
     @Override
     public String toString() {
         return fullName;
+    }
+
+    /**
+     * Takes in a name and compares it with the current name.
+     */
+    @Override
+    public int compareTo(Name name) {
+        return this.fullName.compareTo(name.fullName);
     }
 
     @Override
