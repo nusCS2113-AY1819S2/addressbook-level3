e0d7f54014bb6a6a8489d73af68d1780f2dc1d73
truegitnovice <e0191784@u.nus.edu>
2019-02-28 15:03:13 +0800

    LogicTest: add logic test for 'sortByEmail'

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -201,6 +201,24 @@ public class LogicTest {
     }
 
     @Test
+    public void execute_sortByEmail() throws Exception {
+        // prepare expectations
+        TestDataHelper helper = new TestDataHelper();
+        AddressBook expectedAB = helper.generateAddressBook(false, true);
+        expectedAB.sortByEmail();
+        List<? extends ReadOnlyPerson> expectedList = expectedAB.getAllPersons().immutableListView();
+
+        // prepare address book state
+        helper.addToAddressBook(addressBook, false, true);
+
+        assertCommandBehavior("sort",
+                Command.getMessageForPersonSortByEmailShownSummary(expectedList),
+                expectedAB,
+                true,
+                expectedList);
+    }
+
+    @Test
     public void execute_view_invalidArgsFormat() throws Exception {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE);
         assertCommandBehavior("view ", expectedMessage);
