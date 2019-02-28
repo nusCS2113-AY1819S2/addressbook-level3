ca87ffc113700b277c49fdc509f49f0b7045c9a5
truegitnovice <e0191784@u.nus.edu>
2019-02-28 14:43:49 +0800

    commands: add SortByEmailCommand.java

--- src/seedu/addressbook/commands/SortByEmailCommand.java
+++ src/seedu/addressbook/commands/SortByEmailCommand.java
@@ -1,4 +1,26 @@
 package seedu.addressbook.commands;
 
-public class SortByEmailCommand {
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Lists all persons sorted by their emails in the address book to the user.
+ */
+public class SortByEmailCommand extends Command {
+
+    public static final String COMMAND_WORD = "sortByEmail";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
+            + "Displays all persons in the address book sorted by email as a list with index numbers.\n\t"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sortByEmail();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonSortByEmailShownSummary(allPersons), allPersons);
+    }
 }
