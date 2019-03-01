ec496124818bd44e0f37bd95635acd58742d3c53
jingchen-z <z782180503@gmail.com>
2019-03-01 23:03:02 +0800

    Added ParserTest, on LastCommand

--- test/java/seedu/addressbook/parser/ParserTest.java
+++ test/java/seedu/addressbook/parser/ParserTest.java
@@ -59,6 +59,12 @@ public class ParserTest {
     }
 
     @Test
+    public void LastCommand_parsedCorrectly() {
+        final String input = "last";
+        parseAndAssertCommandType(input, LastCommand.class);
+    }
+
+    @Test
     public void exitCommand_parsedCorrectly() {
         final String input = "exit";
         parseAndAssertCommandType(input, ExitCommand.class);
