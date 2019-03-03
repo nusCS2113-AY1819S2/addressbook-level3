6b8ca8015b6db70eecbc2eb09b99664eb6dfd8b5
Windrich <windrichie@gmail.com>
2019-03-03 16:50:39 +0800

    added new tests for the AddListCommand

--- src/seedu/addressbook/commands/AddListCommand.java
+++ src/seedu/addressbook/commands/AddListCommand.java
@@ -20,9 +20,9 @@ Add a person to the addressbook and list down everyone in the addressbook right 
 public class AddListCommand extends Command{
     public static final String COMMAND_WORD = "add&list";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book and list the whole address book after a successful add. "
-            + "Contact details can be marked private by prepending 'p' to the prefix.\n"
-            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds a person to the address book and list the whole address book after a successful add. "
+            + "Contact details can be marked private by prepending 'p' to the prefix.\n\t"
+            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n\t"
             + "Example: " + COMMAND_WORD
             + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
