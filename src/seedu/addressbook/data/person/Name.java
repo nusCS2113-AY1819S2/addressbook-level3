a98393a618ca856a3efc661b65de950df29e53fa
WU PEI HSUAN <ashowzxc@gmail.com>
2019-03-03 14:45:39 +0800

    add sort command

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Represents a Person's name in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
-public class Name {
+public class Name implements Comparable<Name>{
 
     public static final String EXAMPLE = "John Doe";
     public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphanumeric characters";
@@ -60,5 +60,14 @@ public class Name {
     public int hashCode() {
         return fullName.hashCode();
     }
+
+
+    /**compare name for sorting function*/
+    @Override
+    public int compareTo(Name name) {
+        return this.fullName.compareTo(name.fullName);
+    }
+
+
 
 }
