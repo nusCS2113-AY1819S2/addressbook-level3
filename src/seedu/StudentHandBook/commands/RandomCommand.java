efff39c1961dc7255a180724ffd51f670167f63d
yongjia96 <yongjia96@gmail.com>
2019-03-04 19:06:43 +0800

    Workout class implemented

---
+++ src/seedu/StudentHandBook/commands/RandomCommand.java
@@ -0,0 +1,55 @@
+package seedu.StudentHandBook.commands;
+
+import seedu.StudentHandBook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+import java.util.*;
+
+
+public class RandomCommand extends Command {
+
+    public static final String COMMAND_WORD = "random";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
+            + "Display a random person and their details in the address book. \n\t"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+
+    public CommandResult execute() {
+        final List<ReadOnlyPerson> randomPerson = getRandomPerson();
+        if(randomPerson.size() == 0) {
+            return new CommandResult(getMessageForRandomFail());
+        }
+        else {
+            return new CommandResult(getMessageForRandomShown(), randomPerson);
+        }
+    }
+
+
+    private List<ReadOnlyPerson> getRandomPerson() {
+        final List<ReadOnlyPerson> randomEntry = new ArrayList<>();
+        Random random = new Random();
+        List<ReadOnlyPerson> allPerson = addressBook.getAllPersons().immutableListView();
+        int upperBound = allPerson.size();
+        if (upperBound == 0) {
+            return randomEntry;
+        }
+
+        int index = 0;
+        int randomIndex = random.nextInt(upperBound);
+
+        for(ReadOnlyPerson person : allPerson) {
+            if (index == randomIndex) {
+                randomEntry.add(person);
+                break;
+            }
+            index++;
+        }
+        return randomEntry;
+    }
+
+
+
+}
