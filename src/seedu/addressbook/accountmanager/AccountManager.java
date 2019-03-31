bd52498134bd924219c6fc0285aa89c4444f8f71
liguanlong <liguanlongclivian@outlook.com>
2019-03-31 15:54:59 +0800

    Update on Password Security

--- src/seedu/addressbook/accountmanager/AccountManager.java
+++ src/seedu/addressbook/accountmanager/AccountManager.java
@@ -10,13 +10,15 @@ import java.util.Properties;
 public class AccountManager {
 
     private boolean loginStatus;
+    private String currentAccount;
 
     public static final String LOGIN_PROMPT = "Please login or register in order to use the address book";
     private static final String SUCCESS = "Login Success, loading...";
     private static final String REGISTRATION_SUCCEED = "Registration succeed, please login using the username and password";
-    private static final String Invalid_USERNAME_OR_PASSWORD = "Invalid username or password, please try again";
-    private static final String Invalid_FORMAT = "Please login or register using: " + "\n" + "login username password" + "\n" + "register username password";
+    private static final String INVALID_USERNAME_OR_PASSWORD = "Invalid username or password, please try again";
+    private static final String INVALID_FORMAT = "Please login or register using: " + "\n" + "login username password" + "\n" + "register username password";
     private static final String USERNAME_REGISTERED = "The username has already been registered, please try a new username";
+    private static final String WEAK_PASSWORD = "The password should contain at least one lowercase letter, one uppercase letter, and one digit";
     private Map<String, String> accounts = new HashMap<String, String>();
 
 
@@ -33,44 +35,47 @@ public class AccountManager {
         return loginStatus;
     }
 
+    public String getCurrentAccount(){
+        return currentAccount;
+    }
+
     public String accountCommandHandler(String userCommandText){
         String[] accountInfo = userCommandText.split(" ");
         try {
             if (accountInfo.length == 3 && accountInfo[0].equals("login")) {
-                if (accounts.containsKey(accountInfo[1]) && accounts.get(accountInfo[1]).equals(accountInfo[2])) {
+                if (this.accounts.containsKey(accountInfo[1]) && this.accounts.get(accountInfo[1]).equals(accountInfo[2])) {
+                    this.currentAccount = accountInfo[1];
                     this.loginStatus = true;
                     return SUCCESS;
                 }
                 else {
-                    return Invalid_USERNAME_OR_PASSWORD;
+                    return INVALID_USERNAME_OR_PASSWORD;
                 }
             }
             else{
                 if (accountInfo.length == 3 && accountInfo[0].equals("register")){
                     String username = accountInfo[1];
-                    if (accounts.containsKey(accountInfo[1]))
-                    {
-                        return(USERNAME_REGISTERED);
+                    if (this.accounts.containsKey(accountInfo[1])) {
+                        return USERNAME_REGISTERED;
                     }
                     String password = accountInfo[2];
+                    if (weakPassword(password)){
+                        return WEAK_PASSWORD;
+                    }
                     register(username, password);
                     return REGISTRATION_SUCCEED;
                 }
                 else{
-                    return Invalid_FORMAT;
+                    return INVALID_FORMAT;
                 }
             }
         }
         catch (Exception e){
-            return Invalid_FORMAT;
+            return INVALID_FORMAT;
         }
     }
 
-    private void register(String username, String password){
-        accounts.put(username, password);
-    }
-    public void storeAccounts()
-    {
+    public void storeAccounts() {
         try {
             Properties accountsFile = new Properties();
             for (Map.Entry<String, String> entry: this.accounts.entrySet()){
@@ -83,8 +88,8 @@ public class AccountManager {
         }
 
     }
-    private void loadAccounts()
-    {
+
+    private void loadAccounts() {
         try {
             Properties accountsFile = new Properties();
             accountsFile.load(new FileInputStream("accounts.properties"));
@@ -95,5 +100,23 @@ public class AccountManager {
         {
 
         }
+    }
+
+    private void register(String username, String password){
+        this.accounts.put(username, password);
+        storeAccounts();
+    }
+
+    private boolean weakPassword(String password){
+        return password.toLowerCase().equals(password) || doesNotContainDigit(password);
+    }
+
+    private boolean doesNotContainDigit(String password){
+        for (char character : password.toCharArray()) {
+            if (Character.isDigit(character)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- test/java/seedu/addressbook/accountmanager/AccountManagerTest.java
+++ test/java/seedu/addressbook/accountmanager/AccountManagerTest.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.*;
 public class AccountManagerTest {
 
     private static final String SUCCESS = "Login Success, loading...";
-    private static final String Invalid_USERNAME_OR_PASSWORD = "Invalid username or password, please try again";
+    private static final String INVALID_USERNAME_OR_PASSWORD = "Invalid username or password, please try again";
     private static final String REGISTRATION_SUCCEED = "Registration succeed, please login using the username and password";
-    private static final String Invalid_FORMAT = "Please login or register using: " + "\n" + "login username password" + "\n" + "register username password";
+    private static final String INVALID_FORMAT = "Please login or register using: " + "\n" + "login username password" + "\n" + "register username password";
     private static final String USERNAME_REGISTERED = "The username has already been registered, please try a new username";
-    private AccountManager accountManager = new AccountManager();
+    private static final String WEAK_PASSWORD = "The password should contain at least one lowercase letter, one uppercase letter, and one digit";    private AccountManager accountManager = new AccountManager();
 
     @Test
     public void setLoginStatus() {
@@ -27,17 +27,21 @@ public class AccountManagerTest {
 
     @Test
     public void checkLoginInfo() {
-        final String input1 = "login guanlong 12345";
+        final String input1 = "login Admin Admin123";
         final String input2 = "some_random_string";
         final String input3 = "login wrong_username wrong_password";
-        final String input4 = "register guanlong 123";
-        final String input5 = "register new new";
-        final String input6 = "login new new";
+        final String input4 = "register new new123";
+        final String input5 = "register new New";
+        final String input6 = "register new New123";
+        final String input7 = "register Admin Admin345";
+        final String input8 = "login new New123";
         assertEquals(accountManager.accountCommandHandler(input1), SUCCESS);
-        assertEquals(accountManager.accountCommandHandler(input2), Invalid_FORMAT);
-        assertEquals(accountManager.accountCommandHandler(input3), Invalid_USERNAME_OR_PASSWORD);
-        assertEquals(accountManager.accountCommandHandler(input4), USERNAME_REGISTERED);
-        assertEquals(accountManager.accountCommandHandler(input5), REGISTRATION_SUCCEED);
-        assertEquals(accountManager.accountCommandHandler(input6), SUCCESS);
+        assertEquals(accountManager.accountCommandHandler(input2), INVALID_FORMAT);
+        assertEquals(accountManager.accountCommandHandler(input3), INVALID_USERNAME_OR_PASSWORD);
+        assertEquals(accountManager.accountCommandHandler(input4), WEAK_PASSWORD);
+        assertEquals(accountManager.accountCommandHandler(input5), WEAK_PASSWORD);
+        assertEquals(accountManager.accountCommandHandler(input6), REGISTRATION_SUCCEED);
+        assertEquals(accountManager.accountCommandHandler(input7), USERNAME_REGISTERED);
+        assertEquals(accountManager.accountCommandHandler(input8), SUCCESS);
     }
 }
