b9ecc26c731d96ea1479a040b8acaca935a0c9e9
WU PEI HSUAN <ashowzxc@gmail.com>
2019-04-07 15:18:07 +0800

    Enhancement: for invalid command

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -16,7 +16,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds a person to the address book. "
             + "Contact details can be marked private by prepending 'p' to the prefix.\n\t"
-            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS m/APPOINTMENT d/DOCTOR [t/TAG]...\n\t"
+            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS m/APPOINTMENT d/DOCTOR s/STATUS [t/TAG]...\n\t"
             + "Example: " + COMMAND_WORD
             + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 m/30November d/DOCTOR TAN s/Observation t/friends t/owesMoney";
 
